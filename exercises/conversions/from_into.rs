e56b8cc25cd2d1ca8d73bcc0847a586ad6e9977f
Choi-Jungwoo <ijungwoo02@outlook.com>
2024-09-30 16:06:39 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let mut split = s.split(',').map(str::trim).collect::<Vec<&str>>();
+
+        match split.as_slice() {
+            [name, age_str] if !name.is_empty() => {
+                if let Ok(age) = age_str.parse::<usize>() {
+                    return Person {
+                        name: name.to_string(),
+                        age,
+                    };
+                }
+            }
+            _ => {}
+        }
+
+        Person::default()
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,12 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b) = tuple;
+        let red = r.try_into().map_err(|_| IntoColorError::IntConversion)?;
+        let green = g.try_into().map_err(|_| IntoColorError::IntConversion)?;
+        let blue = b.try_into().map_err(|_| IntoColorError::IntConversion)?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -48,6 +52,8 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let new: (i16, i16, i16) = arr.try_into().map_err(|_| IntoColorError::IntConversion)?;
+        new.try_into()
     }
 }
 
@@ -55,6 +61,12 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let new = (slice[0], slice[1], slice[2]);
+        new.try_into()
     }
 }
 
